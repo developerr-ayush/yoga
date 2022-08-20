be63efab7779c0a5927e136d87adfe960ea496db
shahayush480 <79455386+shahayush480@users.noreply.github.com>
2022-08-20 08:31:26 +0530

    done

---
+++ mail.php
@@ -0,0 +1,33 @@
+<?php
+   // data sent in header are in JSON format
+   header('Content-Type: application/json');
+   // takes the value from variables and Post the data
+   $name = $_POST['name'];
+   $email = $_POST['email'];
+   $message = $_POST['message'];
+   $to = "shahayush480@gmail.com" . $email;
+   $subject = "Contact Us";
+   // Email Template
+   $message .= "<b>Name: </b>".$name."<br>".
+   "<b>Email Address : </b>".$email."<br>".
+   "<b>Email Address : </b>".$message."<br>"
+   
+   ;
+
+   $header = "From:"+$email+" \r\n";
+   $header .= "MIME-Version: 1.0\r\n";
+   $header .= "Content-type: text/html\r\n";
+   $retval = mail ($to,$subject,$message,$header);
+   // message Notification
+   if( $retval == true ) {
+      echo json_encode(array(
+         'success'=> true,
+         'message' => 'Message sent successfully'
+      ));
+   }else {
+      echo json_encode(array(
+         'error'=> true,
+         'message' => 'Error sending message'
+      ));
+   }
+?>
